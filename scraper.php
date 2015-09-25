17acdae84cb9cd2335ebf07fa8459e986a3e6648
mrjoshuasamuel <joshuasamuelmopur@gmail.com>
2015-09-25 12:42:39 +0530

    Modified all the php errors!

--- scraper.php
+++ scraper.php
@@ -1,18 +1,14 @@
-<html>
-</body>
-<?php
 
+<?php
 $city = $_GET['city'];
 
-$city = str_replace(" ","", $city);
+$city = str_replace(" ","",$city);
 
-$contents = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+$contents = file_get_contents("http://www.weather-forecast.com/locations/$city/forecasts/latest");
 
-preg_match('/3 Day Weather Forecast Summary:<\/b>(.*?)<\/span>/s', $contents, $matches);
+preg_match('/ 3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)<\/span>/s', $contents, $matches);
 
 echo $matches[1];
 
 
 ?>
-</body>
-</htm/>
